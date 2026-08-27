0a57f71d66c3e59f2eb3255a826e427ebb4eeb84
Manoranjan16 <pgul7674@gmail.com>
2026-08-27 07:07:05 +0530

    updated file

--- app/schemas.py
+++ app/schemas.py
@@ -1,10 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class SpamRequest(BaseModel):
-    message: str
+    message: str = Field(..., min_length=1)
 
 class SPamResponse(BaseModel):
     message: str
     prediction: str
 
-    
